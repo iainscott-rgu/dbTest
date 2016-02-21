6c39ec45a57ca43b8ff000df5831a596a2486b25
Iain Scott <odyssey3007@hotmail.com>
2016-02-21 03:41:57 +0000

    test2

---
+++ dbconnect.php
@@ -0,0 +1,18 @@
+<html>
+<title>php db query</title>
+<header>
+    <h1>PHP db connection and Query</h1>
+</header>
+<body>
+<?php
+$conn = new PDO ( 'sqlsrv:server = tcp:bbsqldb.database.windows.net,1433;
+Database = SQL_BB\", \"teamdsqldb\", \"{Sql20022016*}');
+
+
+$rows=$db->query('SELECT firstname,surname FROM Customer ORDER BY surname');
+$firstRow=$rows->fetch();
+print"The first results are that {$firstRow['firstname']} goes with ?{$firstRow['surname']}";
+?>
+
+</body>
+</html>
