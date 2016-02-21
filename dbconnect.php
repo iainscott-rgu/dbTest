ca643139e4d3d799c4e450c2266ea901c885ca85
Iain Scott <odyssey3007@hotmail.com>
2016-02-21 05:09:10 +0000

    test2

--- dbconnect.php
+++ dbconnect.php
@@ -5,19 +5,16 @@
 <body>
 <?php
 
-print "The first results are that";
-
 $conn = new PDO ( 'sqlsrv:server = tcp:bbsqldb.database.windows.net,1433;
 Database = SQL_BB\", \"teamdsqldb\", \"{Sql20022016*}');
-
 
 //$rows=$db->query('SELECT bbname, address FROM [B&B]');
 //$firstRow=$rows->fetch();
 //print"The first results are that {$firstRow['firstname']} goes with ?{$firstRow['surname']}";
 
-$stmt = $conn->prepare("SELECT firstname, surname FROM Customer");
+$stmt = $conn->prepare('SELECT firstname, surname FROM [Customer]');
 $stmt->execute();
-print "The first results are that ['firstname'] goes with ['surname']";
+print "The first name ['firstname'] goes with surname ['surname']";
 
 ?>
 
